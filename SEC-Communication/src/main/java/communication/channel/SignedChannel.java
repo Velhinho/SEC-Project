0b5a9715bd2dc2885f5df171255aac1cffa5ddf1
velhinho <goncalo.velhinho420@gmail.com>
2022-03-29 17:57:19 +0100

    Nonce included in messages

--- SEC-Communication/src/main/java/communication/channel/SignedChannel.java
+++ SEC-Communication/src/main/java/communication/channel/SignedChannel.java
@@ -10,8 +10,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.util.Objects;
 
 public class SignedChannel implements Channel {
     private final Socket socket;
@@ -36,14 +39,61 @@ public class SignedChannel implements Channel {
         this.privateKey = privateKey;
     }
 
+    private long askNonce() throws IOException {
+        var writer = new PrintWriter(getSocket().getOutputStream());
+        System.out.println("Asked for nonce");
+        writer.println("nonce pls");
+        writer.flush();
+        var reader = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
+        var nonce = reader.readLine();
+        System.out.println("Read nonce");
+        return Long.parseLong(nonce);
+    }
+
+    private long giveNonce() throws ChannelException {
+        try {
+            var reader = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
+            var line = reader.readLine();
+            System.out.println("Waiting for nonce request");
+            if (Objects.equals(line, "nonce pls")) {
+                var rng = SecureRandom.getInstance("SHA1PRNG");
+                var writer = new PrintWriter(getSocket().getOutputStream());
+                var nonce = rng.nextLong();
+                writer.println(nonce);
+                writer.flush();
+                System.out.println("Gave nonce");
+                return nonce;
+            } else {
+                throw new ChannelException("Didn't ask for nonce");
+            }
+        } catch (IOException | NoSuchAlgorithmException exception) {
+            throw new ChannelException(exception.getMessage());
+        }
+    }
+
+    private JsonObject appendNonce(JsonObject jsonObject, long nonce) {
+        var newJson = new JsonObject();
+        newJson.addProperty("nonce", nonce);
+        newJson.add("jsonObject", jsonObject);
+        return newJson;
+    }
+
+    private JsonObject appendSignature(JsonObject jsonObject, String signature) {
+        var newJson = new JsonObject();
+        newJson.addProperty("signature", signature);
+        newJson.add("jsonWithNonce", jsonObject);
+        return newJson;
+    }
+
     @Override
     public void sendMessage(JsonObject jsonObject) throws ChannelException {
         try {
-            var message = new JsonObject();
+            var nonce = askNonce();
+            var jsonWithNonce = appendNonce(jsonObject, nonce);
+            var signature = StringSignature.sign(jsonWithNonce.toString(), getPrivateKey());
+            var message = appendSignature(jsonWithNonce, signature);
+
             var writer = new PrintWriter(getSocket().getOutputStream());
-            var signature = StringSignature.sign(jsonObject.toString(), getPrivateKey());
-            message.addProperty("signature", signature);
-            message.add("jsonObject", jsonObject);
             writer.println(message);
             writer.flush();
         } catch (Exception exception) {
@@ -55,23 +105,42 @@ public class SignedChannel implements Channel {
      *
      * {"func_call": "check_account", ...}
      *
+     * {"jsonObject": {"func_call": "check_account", ...}, "nonce": 123123}
      *
-     * {"jsonObject": {"func_call": "check_account", ...}, "signature": asdadsd}
+     * {"jsonWithNonce": {"jsonObject": {"func_call": "check_account", ...}, "nonce": 123123}, "signature": asdadsd}
      * */
+
+    private JsonObject unpackSignature(JsonObject message) throws ChannelException, CryptoException {
+        var jsonWithNonce = message.getAsJsonObject("jsonWithNonce");
+        var signature = message.get("signature").getAsString();
+
+        if (StringSignature.verify(jsonWithNonce.toString(), signature, getPublicKey())) {
+            return jsonWithNonce;
+        } else {
+            throw new ChannelException("Wrong signature");
+        }
+    }
+
+    private JsonObject unpackNonce(JsonObject jsonWithNonce, long givenNonce) throws ChannelException {
+        var jsonObject = jsonWithNonce.getAsJsonObject("jsonObject");
+        var receivedNonce = jsonWithNonce.get("nonce").getAsLong();
+
+        if(givenNonce == receivedNonce) {
+            return jsonObject;
+        } else {
+            throw new ChannelException("Wrong nonce");
+        }
+    }
 
     @Override
     public JsonObject receiveMessage() throws ChannelException {
         try {
+            var givenNonce = giveNonce();
             var reader = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
             var message = JsonParser.parseString(reader.readLine()).getAsJsonObject();
-            var jsonObject = message.getAsJsonObject("jsonObject");
-            var signature = message.get("signature").getAsString();
-
-            if (StringSignature.verify(jsonObject.toString(), signature, getPublicKey())) {
-                return jsonObject;
-            } else {
-                throw new ChannelException("Wrong signature");
-            }
+            System.out.println("message: " + message + "\n");
+            var jsonWithNonce = unpackSignature(message);
+            return unpackNonce(jsonWithNonce, givenNonce);
         } catch (IOException | CryptoException exception) {
             throw new ChannelException(exception.getMessage());
         }
