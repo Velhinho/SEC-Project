cdfc7d7b202bb586c96d977c3c64e6fe49ef1c65
Genio-D <diogodias001@gmail.com>
2022-03-30 17:25:58 +0100

    outputting public keys to file on keystore generation

--- SEC-Client/generate_keystore.sh
+++ SEC-Client/generate_keystore.sh
@@ -2,5 +2,8 @@ echo Insert name for keystore file:
 read keyname
 echo Insert password for new keystore:
 read pw
-pathname="src/main/resources/$keyname"
+resourcepath="src/main/resources/"
+pathname="$resourcepath$keyname"
 keytool -genkeypair -keyalg RSA -validity 999 -keystore $pathname -alias mykey -keypass $pw -storepass $pw -dname "cn=Grupo 21, ou=SEC, o=Técnico, c=PT"
+s=$(keytool -list -rfc -keystore $pathname -alias mykey -storepass $pw | openssl x509 -inform pem -pubkey -noout | sed 's/.*KEY-----//' | tr -d "\n\r")
+echo "$keyname,$s" >> ${resourcepath}publickeys.csv
